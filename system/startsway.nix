0ea08f103f5ed183e692d6dde611727488cb21cb
M. Ian Graham <games@miangraham.com>
2021-06-30 14:43:58 +0900

    clean up sway cruft

--- system/startsway.nix
+++ system/startsway.nix
@@ -5,12 +5,9 @@
     destination = "/bin/win";
     executable = true;
     text = ''
-          #! ${pkgs.bash}/bin/bash
+           #! ${pkgs.bash}/bin/bash -e
 
-          # first import environment variables from the login manager
-          systemctl --user import-environment
-          # then start the service
-          exec systemctl --user start sway.service
-  '';
+           sway
+           '';
   }
 )
