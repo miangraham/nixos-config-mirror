68db80d6caf22274b69f18e2d05e495ccb5bbcfe
M. Ian Graham <games@miangraham.com>
2021-10-07 15:18:50 +0900

    nene services, rss-bridge/nginx config

--- box/nene/default.nix
+++ box/nene/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 let
   pkgs = import ../../common/stable.nix {};
   filter-tweets = import ../../../filter-tweets/default.nix { inherit pkgs; };
@@ -8,6 +8,7 @@ in
     ./hardware-configuration.nix
     ../../system/nixos.nix
     ./twitter.nix
+    ./services.nix
   ];
   system.stateVersion = "20.03";
   networking.hostName = "nene";
@@ -17,43 +18,6 @@ in
     "fs.file-max" = 9000000;
     # "fs.inotify.max_user_instances" = 4096;
     # "net.ipv4.tcp_fin_timeout" = 10;
-  };
-
-  # box specific due to ACME, rip
-  services.nginx = {
-    enable = true;
-    user = "nginx";
-    virtualHosts."testlocal.ian.tokyo" = {
-      root = "/var/www";
-      addSSL = true;
-      enableACME = true;
-    };
-  };
-  security.acme = {
-    email = "spamisevil@ijin.net";
-    acceptTerms = true;
-  };
-
-  services.znc = {
-    enable = true;
-    confOptions = {
-      passBlock = "";
-    };
-    # extraFlags = [ "--debug" ];
-  };
-
-  services.searx = {
-    enable = true;
-    settings = {
-      server.port = 8989;
-      server.bind_address = "0.0.0.0";
-      server.secret_key = "@SEARX_SECRET_KEY@";
-    };
-    environmentFile = /home/ian/.config/searx/env;
-  };
-
-  services.rss-bridge = {
-    enable = true;
   };
 
   # virtualisation.oci-containers = {
---
+++ box/nene/services.nix
@@ -0,0 +1,88 @@
+{ config, ... }:
+let
+  pkgs = import ../../common/stable.nix {};
+in
+{
+  services = {
+    # box specific due to ACME, rip
+    nginx = {
+      enable = true;
+      user = "nginx";
+      virtualHosts = {
+        nene = {
+          serverName = "localhost";
+          locations."/" = {
+            return = "404";
+          };
+        };
+
+        "testlocal.ian.tokyo" = {
+          serverName = "testlocal.ian.tokyo";
+          root = "/var/www";
+          addSSL = true;
+          enableACME = true;
+        };
+
+        rss-bridge = {
+          serverName = "rss-bridge";
+          root = "${pkgs.rss-bridge}";
+
+          locations."/" = {
+            tryFiles = "$uri /index.php$is_args$args";
+          };
+
+          locations."~ ^/index.php(/|$)" = {
+            extraConfig = ''
+              include ${pkgs.nginx}/conf/fastcgi_params;
+              fastcgi_split_path_info ^(.+\.php)(/.+)$;
+              fastcgi_pass unix:${config.services.phpfpm.pools.${config.services.rss-bridge.pool}.socket};
+              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+              fastcgi_param RSSBRIDGE_DATA ${config.services.rss-bridge.dataDir};
+            '';
+          };
+        };
+      };
+    };
+
+    rss-bridge = {
+      enable = true;
+      virtualHost = null;
+      whitelist = [
+        "DevTo"
+        "Facebook"
+        "GithubIssue"
+        "Instagram"
+        "Mastodon"
+        "Pixiv"
+        "Reddit"
+        "Twitter"
+        "Vimeo"
+        "Wikipedia"
+        "Youtube"
+      ];
+    };
+
+    searx = {
+      enable = true;
+      settings = {
+        server.port = 8989;
+        server.bind_address = "0.0.0.0";
+        server.secret_key = "@SEARX_SECRET_KEY@";
+      };
+      environmentFile = /home/ian/.config/searx/env;
+    };
+
+    znc = {
+      enable = true;
+      confOptions = {
+        passBlock = "";
+      };
+      # extraFlags = [ "--debug" ];
+    };
+  };
+
+  security.acme = {
+    email = "spamisevil@ijin.net";
+    acceptTerms = true;
+  };
+}
