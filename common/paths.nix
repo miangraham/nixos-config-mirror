4f4d3926dd9c8e2159ad9326ec6e9a6253c8ea84
M. Ian Graham <games@miangraham.com>
2021-10-14 00:57:27 +0900

    version bumps

--- common/paths.nix
+++ common/paths.nix
@@ -7,11 +7,11 @@ in
     sha256 = "112drvixj81vscj8cncmks311rk2ik5gydpd03d3r0yc939zjskg";
   };
   unstable = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/9bf75dd50b7b6d3ce6aaf6563db95f41438b9bdb.tar.gz";
-    sha256 = "0ii3z5v9p21la8gc8l136s5rax932awz7mk757jciai766lp2fhz";
+    url = "https://releases.nixos.org/nixos/unstable/nixos-21.11pre322215.9bf75dd50b7/nixexprs.tar.xz";
+    sha256 = "04bvfkr28xks9qqpn3fixcn8rdlyjhnqnbrg4ynwzqick0j3zcgw";
   };
   emacs-overlay = fetchTarball {
-    url = "https://github.com/nix-community/emacs-overlay/archive/de51ad003df13d41a1107a1ed766eb226ef9382c.tar.gz";
-    sha256 = "1spfd7wx7lz48kk7cs91gjx6zhvlliqc6376rxkc4idfb0q43xih";
+    url = "https://github.com/nix-community/emacs-overlay/archive/1fbdcc136511834878b122a34b6fdb2af5302ddd.tar.gz";
+    sha256 = "03hxkrljm6vdrw903f9rb1biq20886fybxwhm8c7pv0hm6h031dz";
   };
 }
