057f3bbe97caca3825b051bc1b7b4fcc0ca75c6e
M. Ian Graham <games@miangraham.com>
2021-11-02 17:47:21 +0900

    firewall tweak

--- box/futaba/default.nix
+++ box/futaba/default.nix
@@ -13,5 +13,8 @@ in
   networking.interfaces.eno1.useDHCP = true;
   networking.interfaces.wlp0s20f3.useDHCP = true;
 
-  networking.firewall.allowedTCPPorts = [ 22 23 80 443 2222 8384 8443 8989 9090 ];
+  networking.firewall = {
+    allowedTCPPorts = [ 22 80 443 2222 53 8384 8443 8989 9090 ];
+    allowedUDPPorts = [ 53 ];
+  };
 }
