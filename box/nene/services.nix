05dab7bab85fa10d1c3eb35177aa120d139b8e36
M. Ian Graham <games@miangraham.com>
2021-11-19 01:25:08 +0900

    nene: disable rss-bridge

--- box/nene/services.nix
+++ box/nene/services.nix
@@ -44,44 +44,7 @@ in
           addSSL = true;
           enableACME = true;
         };
-
-        rss-bridge = {
-          serverName = "rss-bridge";
-          root = "${pkgs.rss-bridge}";
-
-          locations."/" = {
-            tryFiles = "$uri /index.php$is_args$args";
-          };
-
-          locations."~ ^/index.php(/|$)" = {
-            extraConfig = ''
-              include ${pkgs.nginx}/conf/fastcgi_params;
-              fastcgi_split_path_info ^(.+\.php)(/.+)$;
-              fastcgi_pass unix:${config.services.phpfpm.pools.${config.services.rss-bridge.pool}.socket};
-              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-              fastcgi_param RSSBRIDGE_DATA ${config.services.rss-bridge.dataDir};
-            '';
-          };
-        };
       };
-    };
-
-    rss-bridge = {
-      enable = true;
-      virtualHost = null;
-      whitelist = [
-        "DevTo"
-        "Facebook"
-        "GithubIssue"
-        "Instagram"
-        "Mastodon"
-        "Pixiv"
-        "Reddit"
-        "Twitter"
-        "Vimeo"
-        "Wikipedia"
-        "Youtube"
-      ];
     };
 
     searx = {
