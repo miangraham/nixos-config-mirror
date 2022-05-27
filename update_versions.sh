b9fe48b8ea3f60929be34854d4d6a2cbe0df2f05
M. Ian Graham <games@miangraham.com>
2022-05-27 23:49:01 +0900

    version bump

--- update_versions.sh
+++ update_versions.sh
@@ -2,4 +2,13 @@
 
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ `git status --porcelain` ]]; then
+  echo "Outstanding git changes. Refusing to update."
+  exit 1
+fi
+
 nix flake update
+git add flake.lock
+git commit -m "version bump"
