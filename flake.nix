b9bce1f5955030c2b7660f59ca48280fe20e19f1
M. Ian Graham <games@miangraham.com>
2022-08-15 11:49:27 +0900

    obs: Get wrapOBS from nixpkgs/master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "";
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-22.05"; };
-    unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    # unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    unstable = { url = "github:NixOS/nixpkgs/master"; };
     unstable-small = { url = "github:NixOS/nixpkgs/nixos-unstable-small"; };
     nixos-hardware = { url = "github:NixOS/nixos-hardware/master"; };
     home-manager = { url = "github:nix-community/home-manager/release-22.05"; };
