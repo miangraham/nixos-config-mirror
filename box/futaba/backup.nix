177aae2c68ff3e5f4ceeeea8cdc7947a6715ea29
M. Ian Graham <games@miangraham.com>
2022-09-22 02:40:03 +0900

    futaba music backup to homura

--- box/futaba/backup.nix
+++ box/futaba/backup.nix
@@ -15,6 +15,26 @@ let
         mkdir -p /borg
       '';
     };
+
+    music-to-homura = job {
+      paths = [ "/srv/music" ];
+      startAt = "*-*-* *:47:00";
+      repo = "/home/ian/mounts/homuraborg";
+      user = "ian";
+      doInit = false;
+      preHook = ''
+        mkdir -p /home/ian/mounts/homuraborg
+        /run/current-system/sw/bin/sshfs borg@homura:/share/MD0_DATA/nixborg /home/ian/mounts/homuraborg
+      '';
+      postHook = ''
+        /run/wrappers/bin/fusermount3 -u /home/ian/mounts/homuraborg
+      '';
+      extraArgs = "--lock-wait 30";
+      environment = {
+        BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes";
+        BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK = "yes";
+      };
+    };
   };
 in
 pkgs.lib.recursiveUpdate backup.borgbackup { inherit jobs; }
