8fe3aea9616eea658a1e88b913c96b7428905861
M. Ian Graham <games@miangraham.com>
2022-10-06 00:25:48 +0900

    backup: remove home-to-homura jobs, obviated by rnet

--- system/backup.nix
+++ system/backup.nix
@@ -10,22 +10,4 @@ in
       mkdir -p /borg
     '';
   };
-
-  borgbackup.jobs.home-ian-to-homura = job {
-    repo = "/home/ian/mounts/homuraborg";
-    user = "ian";
-    doInit = false;
-    preHook = ''
-      mkdir -p /home/ian/mounts/homuraborg
-      /run/current-system/sw/bin/sshfs borg@homura:/share/MD0_DATA/nixborg /home/ian/mounts/homuraborg
-    '';
-    postHook = ''
-      /run/wrappers/bin/fusermount3 -u /home/ian/mounts/homuraborg
-    '';
-    extraArgs = "--lock-wait 30";
-    environment = {
-      BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes";
-      BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK = "yes";
-    };
-  };
 }
