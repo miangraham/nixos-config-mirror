23d7d6372f9dc6c046a4e28d8d41598cac8c3506
M. Ian Graham <games@miangraham.com>
2022-10-21 13:55:38 +0900

    update twitch-chat-tui

--- home/twitch-chat-tui.nix
+++ home/twitch-chat-tui.nix
@@ -6,8 +6,8 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromSourcehut {
     owner = "~mian";
     repo = pname;
-    rev = "a97290976c4a98cd92f85b5150c6940f41e74f82";
-    sha256 = "sha256-gabNM1nNFOveMmaL8crYUOcgaIZ74880wYO89ql6lDE=";
+    rev = "fc780a2d073d6480568ecf5e40bae0e0e594d394";
+    sha256 = "sha256-0olgH/VzgS+LDZBqJN7ONI3zPg90bxqSohW8Ww40Erc=";
   };
 
   cargoSha256 = "sha256-Lw9VY3BORIrfE5gZDXUGqjpAQB4aKzWcW+DMAIGFky0=";
