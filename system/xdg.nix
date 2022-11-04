a8397510439372678fcfbf228bc288e48d13477a
M. Ian Graham <games@miangraham.com>
2022-11-04 16:14:59 +0900

    add xdg desktop gtk portal

--- system/xdg.nix
+++ system/xdg.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 {
-  programs.sway.enable = true;
-  xdg.portal.enable = true;
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+  };
 }
