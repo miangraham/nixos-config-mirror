70e0e6be3db0bf0538ce1946d19104e45817aa43
M. Ian Graham <games@miangraham.com>
2022-11-30 11:46:46 +0900

    nene: update hardware-configuration.nix

--- box/nene/hardware-configuration.nix
+++ box/nene/hardware-configuration.nix
@@ -6,7 +6,7 @@
 {
   imports = [ "${modulesPath}/installer/scan/not-detected.nix" ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "uas" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -26,4 +26,7 @@
     ];
 
   nix.maxJobs = lib.mkDefault 32;
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.video.hidpi.enable = lib.mkDefault true;
 }
