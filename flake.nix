cccdb311225ee651c4593cd56dcc553e65c1b9e9
M. Ian Graham <games@miangraham.com>
2022-12-06 22:03:36 +0900

    invid-tweaks-2 -> invid-tweaks-4

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-22.11"; };
     unstable = { url = "github:NixOS/nixpkgs/nixos-22.11"; };
     small = { url = "github:NixOS/nixpkgs/nixos-22.11-small"; };
-    invid-testing = { url = "github:miangraham/nixpkgs/invid-tweaks-2"; };
+    invid-testing = { url = "github:miangraham/nixpkgs/invid-tweaks-4"; };
 
     home-manager = { url = "github:nix-community/home-manager/release-22.11"; inputs.nixpkgs.follows = "nixpkgs"; };
     nixos-hardware = { url = "github:NixOS/nixos-hardware/master"; };
