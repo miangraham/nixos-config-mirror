e05f8b17a0e113734f4f965f6b1300d7854bf574
M. Ian Graham <games@miangraham.com>
2022-12-27 18:28:13 +0900

    freshrss overlay for extensions path

--- common/overlays-stable.nix
+++ common/overlays-stable.nix
@@ -6,7 +6,17 @@ let
       src = inputs.tdlib;
     });
   });
+  freshOverlay = (self: super: {
+    freshrss = super.freshrss.overrideAttrs(old: {
+      overrideConfig = pkgs.writeText "constants.local.php" ''
+        <?php
+          define('DATA_PATH', getenv('FRESHRSS_DATA_PATH'));
+          define('THIRDPARTY_EXTENSIONS_PATH', getenv('FRESHRSS_THIRDPARTY_EXTENSIONS_PATH'));
+      '';
+    });
+  });
 in
 [
   inputs.emacs-overlay.overlay
+  freshOverlay
 ]
