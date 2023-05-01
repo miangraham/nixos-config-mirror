f0a2fe51b01927f7d69b26f363aeb69a111bb3dc
M. Ian Graham <games@miangraham.com>
2023-05-02 02:14:44 +0900

    ranni: try sanoid

--- box/ranni/services.nix
+++ box/ranni/services.nix
@@ -11,6 +11,22 @@ in
       autoScrub.enable = true;
     };
 
+    sanoid = {
+      enable = true;
+      interval = "daily";
+      templates.default = {
+        autosnap = true;
+        hourly = 0;
+        daily = 7;
+        monthly = 1;
+        yearly = 0;
+      };
+      datasets.srv = {
+        recursive = "zfs";
+        useTemplate = [ "default" ];
+      };
+    };
+
     syncthing.guiAddress = "0.0.0.0:8384";
 
     pipewire.enable = pkgs.lib.mkForce false;
