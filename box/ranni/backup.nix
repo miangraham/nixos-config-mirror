9f9967087ee245607ed585b08106d12059a5f68a
M. Ian Graham <games@miangraham.com>
2023-05-10 06:10:58 +0900

    add some ranni backups

--- box/ranni/backup.nix
+++ box/ranni/backup.nix
@@ -5,6 +5,20 @@ let
     inherit pkgs backupTime;
   };
   inherit (import ../../system/backup-utils.nix {inherit pkgs backupTime;}) job;
-  jobs = {};
+  jobs = {
+    home-ian-to-ranni = job {
+      repo = "/srv/borg/ranni";
+      user = "root";
+      startAt = "*-*-* 04:00:00";
+      prune = {
+        keep = {
+          hourly = 0;
+          daily = 7;
+          weekly = 3;
+          monthly = 3;
+        };
+      };
+    };
+  };
 in
 pkgs.lib.recursiveUpdate backup.borgbackup { inherit jobs; }
