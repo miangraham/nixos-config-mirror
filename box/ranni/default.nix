91dd179daf0ad8691c7ff37041eecf5ccc41c093
M. Ian Graham <games@miangraham.com>
2023-06-03 03:04:01 +0900

    ranni: 23.05 upgrade

--- box/ranni/default.nix
+++ box/ranni/default.nix
@@ -75,5 +75,5 @@ in
   security.rtkit.enable = pkgs.lib.mkForce false;
   home-manager.users.ian.services.playerctld.enable = pkgs.lib.mkForce false;
 
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.05";
 }
