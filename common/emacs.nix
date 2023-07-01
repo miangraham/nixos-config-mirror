f82948964076a3e7cb714991a7de2b70e0926404
M. Ian Graham <games@miangraham.com>
2023-07-01 14:33:45 +0900

    emacs: switch package from overlay unstable to emacs29-pgtk

--- common/emacs.nix
+++ common/emacs.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 pkgs.emacsWithPackagesFromUsePackage {
   config = "";
-  package = (pkgs.emacs-unstable.override { withPgtk = true; });
+  package = pkgs.emacs29-pgtk;
   alwaysEnsure = true;
   override = epkgs: epkgs // {
     telega = epkgs.melpaPackages.telega;
