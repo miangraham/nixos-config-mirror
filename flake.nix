8ca41b0c343bcbfa57709948d544cfdf5bee8e67
M. Ian Graham <games@miangraham.com>
2023-07-22 09:00:30 +0900

    unstable: move from nixos-23.05 to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "";
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
-    unstable = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
+    unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
     small = { url = "github:NixOS/nixpkgs/nixos-23.05-small"; };
 
     home-manager = { url = "github:nix-community/home-manager/release-23.05"; inputs.nixpkgs.follows = "nixpkgs"; };
