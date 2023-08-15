ca8b2cbb5a2535646a3efc6fedca39af6db39a16
M. Ian Graham <games@miangraham.com>
2023-08-16 07:59:37 +0900

    futaba: invidious testing

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "";
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
-    unstable = { url = "github:999eagle/nixpkgs?rev=56eef8d0f96d11286cb1681efe953c1e1de3bea6"; };
+    unstable = { url = "github:999eagle/nixpkgs?rev=1a3b7f61b254290c9a7f8faa8a38e255a4c9b53c"; };
     small = { url = "github:NixOS/nixpkgs/nixos-23.05-small"; };
 
     home-manager = { url = "github:nix-community/home-manager/release-23.05"; inputs.nixpkgs.follows = "nixpkgs"; };
