e2c90719324480398168d65ae512d986849e68d1
M. Ian Graham <games@miangraham.com>
2023-11-16 09:11:45 +0900

    futaba conduit testing

--- box/futaba/services.nix
+++ box/futaba/services.nix
@@ -55,11 +55,12 @@ in
     phpfpm.pools.freshrss.phpEnv.FRESHRSS_THIRDPARTY_EXTENSIONS_PATH = "/srv/freshrss/extensions";
 
     matrix-conduit = {
-      enable = false;
+      enable = true;
+      package = unstable.matrix-conduit;
       settings.global = {
         server_name = "graham.tokyo";
         database_backend = "rocksdb";
-        allow_federation = false;
+        allow_federation = true;
         allow_registration = false;
       };
     };
@@ -102,13 +103,13 @@ in
     nginx = {
       enable = true;
       user = "nginx";
-      # upstreams = {
-      #   "backend_conduit" = {
-      #     servers = {
-      #       "[::1]:${toString config.services.matrix-conduit.settings.global.port}" = {};
-      #     };
-      #   };
-      # };
+      upstreams = {
+        "backend_conduit" = {
+          servers = {
+            "[::1]:${toString config.services.matrix-conduit.settings.global.port}" = {};
+          };
+        };
+      };
       virtualHosts = {
         futaba = {
           serverName = "192.168.0.128";
@@ -131,65 +132,65 @@ in
             charset utf-8;
           '';
         };
-        # "graham.tokyo" = {
-        #   serverName = "graham.tokyo";
-        #   forceSSL = true;
-        #   enableACME = true;
-          # listen = [
-          #   {
-          #     addr = "0.0.0.0";
-          #     port = 443;
-          #     ssl = true;
-          #   }
-          #   {
-          #     addr = "[::]";
-          #     port = 443;
-          #     ssl = true;
-          #   }
-          #   {
-          #     addr = "0.0.0.0";
-          #     port = 8448;
-          #     ssl = true;
-          #   }
-          #   {
-          #     addr = "[::]";
-          #     port = 8448;
-          #     ssl = true;
-          #   }
-          # ];
-          # extraConfig = ''
-          #   merge_slashes off;
-          # '';
-          # locations."/_matrix/" = {
-          #   proxyPass = "http://backend_conduit$request_uri";
-          #   proxyWebsockets = true;
-          #   extraConfig = ''
-          #     proxy_set_header Host $host;
-          #     proxy_buffering off;
-          #   '';
-          # };
-          # locations."=/.well-known/matrix/server" = {
-          #   # Use the contents of the derivation built previously
-          #   alias = "${well_known_server}";
+        "graham.tokyo" = {
+          serverName = "graham.tokyo";
+          forceSSL = true;
+          enableACME = true;
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 443;
+              ssl = true;
+            }
+            {
+              addr = "[::]";
+              port = 443;
+              ssl = true;
+            }
+            {
+              addr = "0.0.0.0";
+              port = 8448;
+              ssl = true;
+            }
+            {
+              addr = "[::]";
+              port = 8448;
+              ssl = true;
+            }
+          ];
+          extraConfig = ''
+            merge_slashes off;
+          '';
+          locations."/_matrix/" = {
+            proxyPass = "http://backend_conduit$request_uri";
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_buffering off;
+            '';
+          };
+          locations."=/.well-known/matrix/server" = {
+            # Use the contents of the derivation built previously
+            alias = "${well_known_server}";
 
-          #   extraConfig = ''
-          #     # Set the header since by default NGINX thinks it's just bytes
-          #     default_type application/json;
-          #   '';
-          # };
-          # locations."=/.well-known/matrix/client" = {
-          #   # Use the contents of the derivation built previously
-          #   alias = "${well_known_client}";
+            extraConfig = ''
+              # Set the header since by default NGINX thinks it's just bytes
+              default_type application/json;
+            '';
+          };
+          locations."=/.well-known/matrix/client" = {
+            # Use the contents of the derivation built previously
+            alias = "${well_known_client}";
 
-          #   extraConfig = ''
-          #     # Set the header since by default NGINX thinks it's just bytes
-          #     default_type application/json;
+            extraConfig = ''
+              # Set the header since by default NGINX thinks it's just bytes
+              default_type application/json;
 
-          #     # https://matrix.org/docs/spec/client_server/r0.4.0#web-browser-clients
-          #     add_header Access-Control-Allow-Origin "*";
-          #   '';
-          # };
-        # };
+              # https://matrix.org/docs/spec/client_server/r0.4.0#web-browser-clients
+              add_header Access-Control-Allow-Origin "*";
+            '';
+          };
+        };
       };
     };
 
