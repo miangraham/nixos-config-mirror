b78f611b8b153751b180748885f0720c0c1b2df4
M. Ian Graham <games@miangraham.com>
2023-12-01 22:00:01 +0900

    version bump

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "";
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
-    unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    unstable = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
     small = { url = "github:NixOS/nixpkgs/nixos-23.11-small"; };
 
     nixos-hardware = { url = "github:NixOS/nixos-hardware/master"; };
