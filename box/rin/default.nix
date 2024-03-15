2b64085db74104f4ff9f0662bd865bb11c39cef1
M. Ian Graham <games@miangraham.com>
2024-03-15 11:07:37 -0700

    rin: nebula travel tweaks

--- box/rin/default.nix
+++ box/rin/default.nix
@@ -6,6 +6,7 @@ in
   imports = [
     ./hardware-configuration.nix
     ../../system
+    ../../system/nebula-node.nix
     ../../common/desktop.nix
   ];
 
@@ -21,6 +22,25 @@ in
       enp5s0.useDHCP = true;
       wlp3s0.useDHCP = true;
     };
+    extraHosts = pkgs.lib.mkForce ''
+192.168.0.108 yuno
+192.168.0.110 homura
+192.168.0.111 nanachi
+192.168.100.117 ranni
+192.168.100.119 pika
+192.168.0.120 boxypi
+192.168.100.127 fuuka
+192.168.0.127 nextcloud
+192.168.100.128 futaba
+192.168.100.128 invid
+192.168.100.128 freshrss
+192.168.0.128 graham.tokyo
+192.168.0.129 maho
+192.168.0.132 nene
+192.168.0.133 rin
+192.168.0.155 tinypi
+192.168.0.167 bocchi
+'';
   };
   systemd.services.NetworkManager-wait-online.enable = false;
 
