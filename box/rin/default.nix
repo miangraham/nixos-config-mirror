cb3a29db22e58106e3b2018f075220a74373088a
M. Ian Graham <games@miangraham.com>
2024-03-21 17:46:26 +0900

    rin: hosts overrides off

--- box/rin/default.nix
+++ box/rin/default.nix
@@ -22,25 +22,7 @@ in
       enp5s0.useDHCP = true;
       wlp3s0.useDHCP = true;
     };
-    extraHosts = pkgs.lib.mkForce ''
-192.168.0.108 yuno
-192.168.0.110 homura
-192.168.0.111 nanachi
-192.168.100.117 ranni
-192.168.100.119 pika
-192.168.0.120 boxypi
-192.168.100.127 fuuka
-192.168.0.127 nextcloud
-192.168.100.128 futaba
-192.168.100.128 invid
-192.168.100.128 freshrss
-192.168.0.128 graham.tokyo
-192.168.0.129 maho
-192.168.100.132 nene
-192.168.0.133 rin
-192.168.0.155 tinypi
-192.168.0.167 bocchi
-'';
+    # extraHosts = pkgs.lib.mkForce '''';
   };
   systemd.services.NetworkManager-wait-online.enable = false;
 
