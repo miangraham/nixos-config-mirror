a6c37fa91050d3570982979162d17f323dfe93a8
M. Ian Graham <games@miangraham.com>
2024-05-05 17:56:22 +0900

    nano headless wayvnc first working version

--- box/nano/default.nix
+++ box/nano/default.nix
@@ -9,89 +9,59 @@
 
   boot.blacklistedKernelModules = [ ];
 
-  powerManagement = {
-    cpuFreqGovernor = "schedutil";
-    # powertop.enable = true;
-  };
+  environment = {
+    systemPackages = [
+      pkgs.dbus
+    ];
 
-  # hardware.deviceTree = {
-  #   name = "rockchip/rk3588s-nanopi-r6c.dtb";
-  #   overlays = [];
-  # };
-
-  environment.systemPackages = [
-    pkgs.dbus
-  ];
-
-  environment.variables = {
-    WLR_BACKENDS = "headless";
-    WLR_LIBINPUT_NO_DEVICES = "1";
-    WAYLAND_DISPLAY = "wayland-1";
+    variables = {
+      WLR_BACKENDS = "headless";
+      WLR_LIBINPUT_NO_DEVICES = "1";
+      WAYLAND_DISPLAY = "wayland-1";
+    };
   };
 
   services.dbus.enable = true;
 
-  systemd.services.sway = {
-    serviceConfig = {
-      Type = "simple";
-      # Type = "dbus";
-      User = "ian";
-      # BusName = "com.miangraham.sway";
+  systemd.services = {
+    sway = {
+      serviceConfig = {
+        Type = "simple";
+        User = "ian";
+      };
+      wantedBy = [ "multi-user.target" ];
+      bindsTo = [ "user@1000.service" ];
+      after = [ "user@1000.service" ];
+      environment = {
+        DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
+        XDG_RUNTIME_DIR = "/run/user/1000";
+      };
+      path = [
+        pkgs.dbus
+      ];
+      script = ''
+        set -ex
+        source /etc/profile
+        source /home/ian/.profile
+        env
+        dbus-update-activation-environment --systemd --all
+        dbus-run-session /etc/profiles/per-user/ian/bin/sway
+      '';
     };
-    wantedBy = [ "multi-user.target" ];
-    bindsTo = [ "user@1000.service" ];
-    after = [ "user@1000.service" ];
-    # wants = [ "user-runtime-dir@1000.service" ];
-    # after = [ "user-runtime-dir@1000.service" ];
-    # wants = [ "dbus.service" "dbus.socket" ];
-    # after = [ "dbus.service" "dbus.socket" ];
-    environment = {
-      DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
-      XDG_RUNTIME_DIR = "/run/user/1000";
-    };
-    path = [
-      pkgs.dbus
-    ];
-    script = ''
-      set -ex
-      source /etc/profile
-      source /home/ian/.profile
-      env
-      ls -lh /run/user
-      ls -lh /run/user/1000
-
-      # echo "launch go"
-      # export $(dbus-launch)
-      # ls -lh /run/user/1000
-      env
-      dbus-update-activation-environment --systemd --all
-      dbus-run-session /etc/profiles/per-user/ian/bin/sway
-    '';
   };
 
-  # systemd.services.wayvnc = {
-
-  # };
-      #   systemd.user.services.waybar = {
-      #   Unit = {
-      #     Description =
-      #       "Highly customizable Wayland bar for Sway and Wlroots based compositors.";
-      #     Documentation = "https://github.com/Alexays/Waybar/wiki";
-      #     PartOf = [ "graphical-session.target" ];
-      #     After = [ "graphical-session-pre.target" ];
-      #   };
-
-      #   Service = {
-      #     ExecStart = "${cfg.package}/bin/waybar";
-      #     ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
-      #     Restart = "on-failure";
-      #     KillMode = "mixed";
-      #   };
-
-      #   Install = { WantedBy = [ cfg.systemd.target ]; };
-      # };
+  home-manager.users.ian.systemd.user.services.wayvnc = {
+    Unit = {
+      Requires = [ "sway-session.target" ];
+      After = [ "sway-session.target" ];
+    };
+    Install.WantedBy = [ "sway-session.target" ];
+    Service.ExecStart = "${pkgs.wayvnc}/bin/wayvnc 127.0.0.1";
+  };
 
   home-manager.users.ian.services.kanshi.enable = pkgs.lib.mkForce false;
+
+  powerManagement.cpuFreqGovernor = "schedutil";
 
   system.stateVersion = "23.11";
 }
