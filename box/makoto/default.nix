ddb8d6f7f65e3767ee685f01ca4284973c52794a
M. Ian Graham <games@miangraham.com>
2024-10-27 14:02:57 +0900

    makoto: enable nebula

--- box/makoto/default.nix
+++ box/makoto/default.nix
@@ -4,7 +4,7 @@
     ./hardware-configuration.nix
     ../../system
     ../../system/home-network-only.nix
-    # ../../system/nebula-node.nix
+    ../../system/nebula-node.nix
     ./services.nix
     # ./containers.nix
   ];
@@ -17,9 +17,8 @@
     };
   };
 
-  # services.nebula.networks.asgard.firewall.inbound = [
-  #   { port = 3001; proto = "tcp"; host = "any"; } # immich
-  # ];
+  services.nebula.networks.asgard.firewall.inbound = [
+  ];
 
   powerManagement = {
     cpuFreqGovernor = "conservative";
