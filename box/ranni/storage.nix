c3bf665d4f024e780b0ce1d5259f1f82e70a18f2
M. Ian Graham <games@miangraham.com>
2024-12-02 18:05:46 +0900

    ranni: update confs for 24.11

--- box/ranni/storage.nix
+++ box/ranni/storage.nix
@@ -36,18 +36,17 @@
     samba = {
       enable = true;
       openFirewall = true;
-      securityType = "user";
-      extraConfig = ''
-        workgroup = WORKGROUP
-        server string = ranni
-        netbios name = ranni
-        security = user
-        hosts allow = 192.168.0. 127.0.0.1 localhost
-        hosts deny = 0.0.0.0/0
-        guest account = nobody
-        map to guest = bad user
-      '';
-      shares = {
+      settings = {
+        global = {
+          "server string" = "ranni";
+          "netbios name" = "ranni";
+          "workgroup" = "WORKGROUP";
+          "security" = "user";
+          "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+          "hosts deny" = "0.0.0.0/0";
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
+        };
         timemachine = {
           path = "/srv/timemachine";
           public = "no";
