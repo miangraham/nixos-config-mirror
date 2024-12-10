c086aa793d2c8aeda081d96c6a8f32bf332405b3
M. Ian Graham <games@miangraham.com>
2024-12-10 16:51:51 +0900

    nene: kernel back to 6.12, oomd slices on

--- box/nene/default.nix
+++ box/nene/default.nix
@@ -25,14 +25,18 @@
     firewall.allowedTCPPorts = [ 80 443 5672 6379 8443 41641 ];
   };
 
-  # temp pin, system locked on 2024-12-08. kernel or bios?
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_6;
-  boot.kernel.sysctl = {
-    "fs.file-max" = 9000000;
-    "net.ipv4.tcp_fin_timeout" = 10;
-  };
+  # boot.kernel.sysctl = {
+  #   "fs.file-max" = 9000000;
+  #   "net.ipv4.tcp_fin_timeout" = 10;
+  # };
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  # temp, system locked on 2024-12-08. bios?
+  systemd.oomd = {
+    enableSystemSlice = true;
+    enableUserSlices = true;
+  };
 
   home-manager.users.ian.home.packages = with pkgs; [];
 
